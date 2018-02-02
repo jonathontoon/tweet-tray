bf1f870a487a67aadbe8894aa73bdab2dc378a4c
Jonathon Toon <jonathontoon@gmail.com>
2018-02-02 19:57:52 +0900

    remove notification image for osx

--- app/utils/Notifier.js
+++ app/utils/Notifier.js
@@ -1,12 +1,12 @@
 const { remote, } = window.require('electron');
 const { app, } = remote;
 
-const Notifier = (title, body, isSilent = false, icon, callback) => {
+const Notifier = (title, body, isSilent = false, iconImage, callback) => {
   app.setAppUserModelId('org.jonathontoon.tweettray');
   const notification = new Notification(title, {
     body,
     silent: isSilent,
-    icon,
+    icon: process.platform === 'darwin' ? null : iconImage,
   });
   notification.onclick = () => {
     callback();
