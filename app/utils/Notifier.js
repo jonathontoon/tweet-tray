9a70a685698c941c3a38d9e31cb6c5303095ba2d
Jonathon Toon <jonathontoon@gmail.com>
2018-02-02 18:35:14 +0900

    add icon to notification

--- app/utils/Notifier.js
+++ app/utils/Notifier.js
@@ -1,11 +1,27 @@
+import path from 'path';
+
 const { remote, } = window.require('electron');
 const { app, } = remote;
+
+const appIconImage = () => {
+  let appIconImagePath = path.join(__dirname, '../resources/1024x1024.png');
+  if (process.platform === 'darwin') {
+    appIconImagePath = path.join(__dirname, '../resources/icon.icns');
+  }
+
+  if (process.platform === 'win32') {
+    appIconImagePath = path.join(__dirname, '../resources/icon.ico');
+  }
+
+  return appIconImagePath;
+};
 
 const Notifier = (title, body, isSilent = false, callback) => {
   app.setAppUserModelId('org.jonathontoon.tweettray');
   const notification = new Notification(title, {
     body,
     silent: isSilent,
+    icon: appIconImage(),
   });
   notification.onclick = () => {
     callback();
