a788527099f28b753f8ea7aa8f103cf215a15644
Jonathon Toon <jonathontoon@gmail.com>
2018-02-23 13:10:33 +0900

    icon button test

---
+++ test/components/IconButton.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Enzyme, { shallow, } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+import IconButton from '../../app/components/IconButton';
+
+import SettingsIcon from '../../resources/settings.svg';
+
+Enzyme.configure({ adapter: new Adapter(), });
+
+const mockCallBack = jest.fn();
+
+function setup() {
+  const component = shallow(<IconButton
+    iconSrc={SettingsIcon}
+    altText="Settings Icon"
+    onClick={mockCallBack}
+  />);
+  return {
+    component,
+  };
+}
+
+describe('IconButton component', () => {
+  it('should render button with image', () => {
+    const { component, } = setup();
+    expect(shallowToJson(component)).toMatchSnapshot();
+  });
+});
