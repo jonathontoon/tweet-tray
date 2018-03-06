e988996b54195934620778aaad2c4ae9e746e6a3
Jonathon Toon <jonathontoon@gmail.com>
2018-03-06 20:55:16 +0900

    new tests

--- test/components/Footer.spec.js
+++ test/components/Footer.spec.js
@@ -2,25 +2,23 @@ import React from 'react';
 import Enzyme, { shallow, } from 'enzyme';
 import { shallowToJson, } from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
-import Header from '../../app/components/Header';
+import Footer from '../../app/components/Footer';
 
 Enzyme.configure({ adapter: new Adapter(), });
 
 function setup() {
-  const component = shallow(<Header
+  const component = shallow(<Footer
     leftView={<div>Left View</div>}
-    title="Header Title"
     rightView={<div>Right View</div>}
   />);
   return {
     component,
-    leftView: component.find('Header__LeftViewStyle').find('div'),
-    title: component.find('Header__TitleStyle'),
-    rightView: component.find('Header__RightViewStyle').find('div'),
+    leftView: component.find('Footer__LeftViewStyle').find('div'),
+    rightView: component.find('Footer__RightViewStyle').find('div'),
   };
 }
 
-describe('Header component', () => {
+describe('Footer component', () => {
   it('should render', () => {
     const { component, } = setup();
     expect(shallowToJson(component)).toMatchSnapshot();
@@ -29,11 +27,6 @@ describe('Header component', () => {
   it('should render with leftView content', () => {
     const { leftView, } = setup();
     expect(shallowToJson(leftView)).toMatchSnapshot();
-  });
-
-  it('should render with title content', () => {
-    const { title, } = setup();
-    expect(title.text()).toMatchSnapshot();
   });
 
   it('should render with rightView content', () => {
