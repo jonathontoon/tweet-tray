63384ba1d27cda9cf0faaabb20b05e09582f2a44
Jonathon Toon <jonathontoon@gmail.com>
2018-03-08 16:52:01 +0900

    add locales to manager

--- app/utils/LocaleManager.js
+++ app/utils/LocaleManager.js
@@ -7,6 +7,19 @@ import englishNZ from '../localizations/en-NZ.json';
 import englishUS from '../localizations/en-US.json';
 import englishZA from '../localizations/en-ZA.json';
 
+import german from '../localizations/de.json';
+import germanDE from '../localizations/de-DE.json';
+import germanCH from '../localizations/de-CH.json';
+import germanAT from '../localizations/de-AT.json';
+
+import korean from '../localizations/ko.json';
+import swedish from '../localizations/sv.json';
+
+import chinese from '../localizations/zh.json';
+import chineseCN from '../localizations/zh-CN.json';
+import chineseTW from '../localizations/zh-TW.json';
+
+
 class LocaleManager {
   constructor() {
     return new LocalizedStrings({
@@ -16,6 +29,15 @@ class LocaleManager {
       'en-NZ': englishNZ,
       'en-US': englishUS,
       'en-ZA': englishZA,
+      de: german,
+      'de-DE': germanDE,
+      'de-CH': germanCH,
+      'de-AT': germanAT,
+      ko: korean,
+      sv: swedish,
+      zh: chinese,
+      'zh-CN': chineseCN,
+      'zh-TW': chineseTW,
     });
   }
 }
