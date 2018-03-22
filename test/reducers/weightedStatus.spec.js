c277e26659db0ddba8acd57cbcfa485c1b265650
Jonathon Toon <jonathontoon@gmail.com>
2018-03-22 23:27:21 +0900

    update test

--- test/reducers/weightedStatus.spec.js
+++ test/reducers/weightedStatus.spec.js
@@ -4,7 +4,16 @@ import * as testData from '../testData';
 
 describe('weightedStatus reducer', () => {
   it('should handle initial state', () => {
-    expect(weightedStatus(undefined, {})).toEqual(null);
+    expect(weightedStatus(undefined, {})).toEqual({
+      text: '',
+      weightedLength: 0,
+      permillage: 0,
+      valid: true,
+      displayRangeStart: 0,
+      displayRangeEnd: 0,
+      validDisplayRangeStart: 0,
+      validDisplayRangeEnd: 0,
+    });
   });
 
   it('should handle UPDATE_WEIGHTED_STATUS', () => {
